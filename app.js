247c30c8a51c04a8adf8e07c785688ce6903bd33
arihantjain19 <arihantjain1905@gmail.com>
2026-08-31 22:31:45 +0530

    Update app.js

--- app.js
+++ app.js
@@ -451,7 +451,7 @@ const SUBMIT_ENDPOINT = "https://script.google.com/macros/s/AKfycbyb0Cl_1MJKPtD3
     submitBtn.textContent = "Submitting…";
 
     // If no real endpoint configured, show info toast
-    if (!SUBMIT_ENDPOINT || SUBMIT_ENDPOINT === "https://script.google.com/macros/s/AKfycbyb0Cl_1MJKPtD33kxp_lcFLGwX98FC3s-SeNep1TOWPVI3doEVE4b-TF61InX-LvXT/exec") {
+    if (!SUBMIT_ENDPOINT || SUBMIT_ENDPOINT === "YOUR_GOOGLE_APPS_SCRIPT_WEB_APP_URL_HERE") {
       submitBtn.disabled = false;
       submitBtn.textContent = "Submit Prototype";
       closeModal('submitModal');
